778a3dd1efd453090944c4ce85eb502045d86203
Tobias Bengtsson <bo.tobias.bengtsson@gmail.com>
2021-12-01 06:30:32 +0100

    Day 1

--- src/Aoc/Day1/Part1.hs
+++ src/Aoc/Day1/Part1.hs
@@ -1,6 +1,15 @@
 module Aoc.Day1.Part1 where
 
-import Data.ByteString
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+import Data.Maybe
 
-solve :: [ByteString] -> String
-solve = undefined
+solve :: [BS.ByteString] -> String
+solve lines = show $ solve' $ fmap (fst . fromJust . BC.readInt) lines
+
+solve' :: [Int] -> Int
+solve' [] = 0
+solve' [_] = 0
+solve' (i:is)
+  | i < head is  = 1 + solve' is
+  | otherwise    = solve' is
--- src/Aoc/Day1/Part2.hs
+++ src/Aoc/Day1/Part2.hs
@@ -1,7 +1,20 @@
 module Aoc.Day1.Part2 where
 
-import Data.ByteString
-import Aoc.Day1.Part1
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+import Data.Maybe
 
-solve :: [ByteString] -> String
-solve = undefined
+solve :: [BS.ByteString] -> String
+solve lines = show $ solve' $ fmap (fst . fromJust . BC.readInt) lines
+
+solve' :: [Int] -> Int
+solve' [] = 0
+solve' [_] = 0
+solve' [_, _] = 0
+solve' [_, _, _] = 0
+solve' (i:is)
+  | fstWindow < sndWindow = 1 + solve' is
+  | otherwise             = solve' is
+  where
+    fstWindow = sum (i:(take 2 is))
+    sndWindow = sum (take 3 is)
