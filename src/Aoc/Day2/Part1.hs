950ad0c074a5f6be43e67ffc49219812927a400a
Tobias Bengtsson <bo.tobias.bengtsson@gmail.com>
2021-12-02 06:47:25 +0100

    Day 2

--- src/Aoc/Day2/Part1.hs
+++ src/Aoc/Day2/Part1.hs
@@ -1,6 +1,36 @@
 module Aoc.Day2.Part1 where
 
-import Data.ByteString
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+import Data.Maybe
 
-solve :: [ByteString] -> String
-solve = undefined
+data Command =
+    Forward Int
+  | Down Int
+  | Up Int
+  deriving (Show)
+
+data Position = Position {
+    horizontal :: Int
+  , depth      :: Int
+} deriving (Show)
+
+solve :: [BS.ByteString] -> String
+solve = show . answer . (foldl execute $ Position 0 0) . fmap parse
+
+answer :: Position -> Int
+answer p = horizontal p * depth p
+
+parse :: BS.ByteString -> Command
+parse line = case BC.split ' ' line of
+    ["forward", i] -> Forward $ toInt i
+    ["down", i]    -> Down $ toInt i
+    ["up", i]      -> Up $ toInt i
+    otherwise      -> error "Could not parse input"
+  where
+    toInt = fst . fromJust . BC.readInt
+
+execute :: Position -> Command -> Position
+execute p (Forward i) = Position { horizontal = horizontal p + i, depth = depth p}
+execute p (Down i) = Position { horizontal = horizontal p, depth = depth p + i}
+execute p (Up i) = Position { horizontal = horizontal p, depth = depth p - i}
--- src/Aoc/Day2/Part2.hs
+++ src/Aoc/Day2/Part2.hs
@@ -1,7 +1,23 @@
 module Aoc.Day2.Part2 where
 
-import Data.ByteString
-import Aoc.Day2.Part1
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+import Data.Maybe
 
-solve :: [ByteString] -> String
-solve = undefined
+import qualified Aoc.Day2.Part1 as Part1
+
+data AimedPosition = AimedPosition {
+    position :: Part1.Position
+  , aim :: Int
+}
+
+nullAimedPosition = AimedPosition (Part1.Position 0 0) 0
+
+solve :: [BS.ByteString] -> String
+solve = show . Part1.answer . position . (foldl execute nullAimedPosition) . fmap Part1.parse
+
+execute :: AimedPosition -> Part1.Command -> AimedPosition
+execute (AimedPosition (Part1.Position h d) a) (Part1.Forward i) =
+    AimedPosition (Part1.Position (h + i) (d + a * i)) a
+execute (AimedPosition p a) (Part1.Down i) = AimedPosition p (a + i)
+execute (AimedPosition p a) (Part1.Up i) = AimedPosition p (a - i)
