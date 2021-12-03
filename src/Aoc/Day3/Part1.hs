7b2394c44c3a0994232e87db2023cc8c9d7e5f6b
Tobias Bengtsson <bo.tobias.bengtsson@gmail.com>
2021-12-03 08:09:38 +0100

    Day 3: unrefined solution

--- src/Aoc/Day3/Part1.hs
+++ src/Aoc/Day3/Part1.hs
@@ -1,6 +1,26 @@
 module Aoc.Day3.Part1 where
 
-import Data.ByteString
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+import Data.Char (digitToInt)
+import Data.Maybe
+import Numeric
 
-solve :: [ByteString] -> String
-solve = undefined
+solve :: [BS.ByteString] -> String
+solve lines = show $ (binToDec lcbString) * (binToDec $ mcbString lines)
+  where
+    lcbString = fmap (\x -> if x == '1' then '0' else '1') $ mcbString lines
+
+mcbString :: [BS.ByteString] -> String
+mcbString = fmap mostCommonBit . fmap BC.unpack . BC.transpose
+
+mostCommonBit :: String -> Char
+mostCommonBit allBits
+  | bitCount >= zeroCount = '1'
+  | otherwise             = '0'
+  where
+    bitCount = length (filter (== '1') allBits)
+    zeroCount = length allBits - bitCount
+
+binToDec :: Integral a => String -> a
+binToDec = fromJust . fmap fst . listToMaybe . readInt 2 (`elem` ['0','1']) digitToInt
--- src/Aoc/Day3/Part2.hs
+++ src/Aoc/Day3/Part2.hs
@@ -1,7 +1,22 @@
 module Aoc.Day3.Part2 where
 
-import Data.ByteString
-import Aoc.Day3.Part1
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+import Data.List
+import qualified Aoc.Day3.Part1 as P1
 
-solve :: [ByteString] -> String
-solve = undefined
+solve :: [BS.ByteString] -> String
+solve lines = show $ o2genRating * co2ScrubRating
+  where
+    o2genRating = P1.binToDec $ solve' True 0 $ fmap BC.unpack lines
+    co2ScrubRating = P1.binToDec $ solve' False 0 $ fmap BC.unpack lines
+
+solve' :: Bool -> Int -> [String] -> String
+solve' _ _ [] = error "No match found"
+solve' _ _ [x] = x
+solve' b i xs = solve' b (i + 1) $ filter meetCriteria xs
+  where
+    mcb = P1.mostCommonBit ((transpose xs) !! i)
+    meetCriteria line = case b of
+      True ->  line !! i == mcb
+      False -> line !! i /= mcb
