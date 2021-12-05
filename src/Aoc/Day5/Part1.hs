b70b97ac951fd2422deaaeabb2eac7cbadbf4341
Tobias Bengtsson <bo.tobias.bengtsson@gmail.com>
2021-12-05 06:56:51 +0100

    Day 5

--- src/Aoc/Day5/Part1.hs
+++ src/Aoc/Day5/Part1.hs
@@ -1,6 +1,33 @@
 module Aoc.Day5.Part1 where
 
-import Data.ByteString
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.Maybe as Maybe
+import qualified Data.MultiSet as MultiSet
 
-solve :: [ByteString] -> String
-solve = undefined
+type Point = (Int, Int)
+type Line = (Point, Point)
+
+readLine :: BS.ByteString -> Line
+readLine s = (readPoint (head splitStr), readPoint (last splitStr))
+  where
+    splitStr = BC.split ' ' s
+
+readPoint :: BS.ByteString -> Point
+readPoint s = (toInt (head splitStr), toInt (last splitStr))
+  where
+    splitStr = BC.split ',' s
+
+solve :: [BS.ByteString] -> String
+solve = show . MultiSet.foldOccur (\p occ x -> if occ < 2 then x else x + 1) 0 . MultiSet.unions . fmap (MultiSet.fromList . traceLine . readLine)
+
+traceLine :: Line -> [Point]
+traceLine ((x1, y1), (x2, y2))
+  | x1 == x2 && y1 <  y2 = fmap (x1, ) [y1..y2]
+  | x1 == x2             = fmap (x1, ) [y2..y1]
+  | x1 <  x2 && y1 == y2 = fmap (, y1) [x1..x2]
+  |             y1 == y2 = fmap (, y1) [x2..x1]
+  | otherwise            = []
+
+toInt :: BS.ByteString -> Int
+toInt = fst . Maybe.fromJust . BC.readInt
