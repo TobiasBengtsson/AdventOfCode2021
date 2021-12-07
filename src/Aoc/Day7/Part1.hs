f9e7f3e53c42423a637dc0511c5be1a373269642
Tobias Bengtsson <bo.tobias.bengtsson@gmail.com>
2021-12-07 07:29:44 +0100

    Day 7

--- src/Aoc/Day7/Part1.hs
+++ src/Aoc/Day7/Part1.hs
@@ -1,6 +1,17 @@
 module Aoc.Day7.Part1 where
 
-import Data.ByteString
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+import Data.Maybe (fromJust)
 
-solve :: [ByteString] -> String
-solve = undefined
+readCrabs :: BS.ByteString -> [Int]
+readCrabs = fmap (fst . fromJust . BC.readInt) . BC.split ','
+
+cost :: Int -> Int -> Int
+cost lineX subX = abs (subX - lineX)
+
+costSum :: Int -> [Int] -> Int
+costSum lineX = sum . fmap (cost lineX)
+
+solve :: [BS.ByteString] -> String
+solve = show . minimum . (\x -> fmap (`costSum` x) [minimum x..maximum x]) . readCrabs . head
--- src/Aoc/Day7/Part2.hs
+++ src/Aoc/Day7/Part2.hs
@@ -1,7 +1,16 @@
 module Aoc.Day7.Part2 where
 
-import Data.ByteString
-import Aoc.Day7.Part1
+import qualified Aoc.Day7.Part1 as P1
+import qualified Data.ByteString as BS
 
-solve :: [ByteString] -> String
-solve = undefined
+seriesSum :: Int -> Int
+seriesSum n = (n * (n + 1)) `div` 2
+
+cost :: Int -> Int -> Int
+cost lineX subX = seriesSum (abs (subX - lineX))
+
+costSum :: Int -> [Int] -> Int
+costSum lineX = sum . fmap (cost lineX)
+
+solve :: [BS.ByteString] -> String
+solve = show . minimum . (\x -> fmap (`costSum` x) [minimum x..maximum x]) . P1.readCrabs . head
