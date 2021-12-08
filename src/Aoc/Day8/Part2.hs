1f0fcb8daf948494da92a7e98718177a060c560a
Tobias Bengtsson <bo.tobias.bengtsson@gmail.com>
2021-12-08 10:31:08 +0100

    Day 8: magic

--- src/Aoc/Day8/Part2.hs
+++ src/Aoc/Day8/Part2.hs
@@ -5,11 +5,4 @@ import qualified Data.ByteString as BS
 import qualified Aoc.Day8.Part1 as P1
 
 solve :: [BS.ByteString] -> String
-solve = show . sum . map solveRow
-
-solveRow :: BS.ByteString -> Int
-solveRow s = read $ concatMap (show . P1.display . (`P1.mapSignal` charmap)) output
-  where
-    obs = fst $ P1.readRow s
-    output = snd $ P1.readRow s
-    charmap = P1.reduceCompleted $ P1.filterCharCounts obs $ foldr P1.filterEasy P1.allMappings obs
+solve = show . sum . map (read . concatMap show . P1.solveRow)
