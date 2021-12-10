b900ae4f1b9fc8f7ce89036457f2c126d1638cf3
Tobias Bengtsson <bo.tobias.bengtsson@gmail.com>
2021-12-10 07:54:46 +0100

    Day 10: part 2, refactorings

--- src/Aoc/Day10/Part1.hs
+++ src/Aoc/Day10/Part1.hs
@@ -2,31 +2,33 @@ module Aoc.Day10.Part1 where
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BC
-import Data.Either
-import qualified Data.Map as Map
-import Data.Maybe
+import Data.Either (lefts)
+import Data.Maybe (fromMaybe)
 
 solve :: [BS.ByteString] -> String
-solve = show . sum . map score . lefts . map ((`parse` []) . BC.unpack)
+solve = show . sum . map corruptScore . lefts . map ((`parse` []) . BC.unpack)
 
-safeHead [] = Nothing
-safeHead xs = Just (head xs)
+data ParseError = Corrupt Char | Incomplete String
 
-data ParseError = Corrupt Char | Incomplete
+isCorrupt :: ParseError -> Bool
+isCorrupt (Corrupt _) = True
+isCorrupt _           = False
 
-score Incomplete = 0
-score (Corrupt x) = fromJust $ Map.lookup x (Map.fromList $ zip [')', '}', ']', '>'] [3, 1197, 57, 25137])
+corruptScore :: ParseError -> Int
+corruptScore (Corrupt x) = fromMaybe 0 $ lookup x (zip closings [3, 57, 1197, 25137])
+corruptScore _ = 0
 
 parse :: String -> String -> Either ParseError ()
 parse [] [] = Right ()
-parse [] _  = Left Incomplete
+parse [] s  = Left $ Incomplete s
 parse (x:xs) state
-  | x `elem` ['(', '{', '[', '<'] = parse xs (x:state)
-  | x `elem` [')', '}', ']', '>'] =
-    case safeHead state of
-      Just '(' -> if x /= ')' then Left (Corrupt x) else (parse xs (drop 1 state))
-      Just '{' -> if x /= '}' then Left (Corrupt x) else (parse xs (drop 1 state))
-      Just '[' -> if x /= ']' then Left (Corrupt x) else (parse xs (drop 1 state))
-      Just '<' -> if x /= '>' then Left (Corrupt x) else (parse xs (drop 1 state))
-      Nothing  -> Left Incomplete
-      _        -> error "Input error"
+  | x `elem` openings = parse xs (x:state)
+  | x `elem` closings = if not $ matches $ head state
+                      then Left (Corrupt x)
+                      else parse xs (drop 1 state)
+  | otherwise = error "Unrecognized character"
+  where
+    matches o = Just x == lookup o (zip openings closings)
+
+openings = ['(', '[', '{', '<']
+closings = [')', ']', '}', '>']
--- src/Aoc/Day10/Part2.hs
+++ src/Aoc/Day10/Part2.hs
@@ -1,7 +1,22 @@
 module Aoc.Day10.Part2 where
 
-import Data.ByteString
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+import Data.Either (lefts)
+import Data.List (sort)
+import Data.Maybe (fromMaybe)
+
 import Aoc.Day10.Part1
 
-solve :: [ByteString] -> String
-solve = undefined
+solve :: [BS.ByteString] -> String
+solve = show . middle . filter (/= 0) . map incompleteScore . lefts . map ((`parse` []) . BC.unpack)
+
+middle :: [Int] -> Int
+middle xs = sort xs !! (length xs `div` 2)
+
+incompleteScore :: ParseError -> Int
+incompleteScore (Incomplete state) = foldl (\acc c -> acc * 5 + incompleteCharScore c) 0 state
+incompleteScore _ = 0
+
+incompleteCharScore :: Char -> Int
+incompleteCharScore c = fromMaybe 0 $ lookup c $ zip openings [1, 2, 3, 4]
