468f30e03b3658cd979f7fe756ce60f166665ffb
Tobias Bengtsson <bo.tobias.bengtsson@gmail.com>
2021-12-12 07:32:59 +0100

    Day 12

--- src/Aoc/Day12/Part1.hs
+++ src/Aoc/Day12/Part1.hs
@@ -1,6 +1,33 @@
 module Aoc.Day12.Part1 where
 
-import Data.ByteString
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+import Data.Char
+import Data.List
 
-solve :: [ByteString] -> String
-solve = undefined
+type Edge = (String, String)
+type Path = [String]
+
+solve :: [BS.ByteString] -> String
+solve = show . length . allPaths "start" "end" . map readRow
+
+allPaths :: String -> String -> [(String, String)] -> [[String]]
+allPaths start end edges = allPaths' start end edges []
+
+allPaths' :: String -> String -> [Edge] -> Path -> [Path]
+allPaths' start end edges visited
+  | start == end = [end:visited]
+  | start == "start" && not (null visited) = []
+  | isSmallNode start && (start `elem` visited) = []
+  | otherwise = concatMap (\s -> allPaths' s end edges (start:visited)) $ adjNodes start edges
+
+adjNodes :: String -> [(String, String)] -> [String]
+adjNodes n = nub . map (\e -> if fst e == n then snd e else fst e) . filter (\e -> fst e == n || snd e == n)
+
+isSmallNode :: String -> Bool
+isSmallNode = isLower . head
+                
+readRow :: BS.ByteString -> (String, String)
+readRow bs = (head splitString, splitString !! 1)
+  where splitString = map BC.unpack $ BC.split '-' bs 
+
--- src/Aoc/Day12/Part2.hs
+++ src/Aoc/Day12/Part2.hs
@@ -1,7 +1,21 @@
 module Aoc.Day12.Part2 where
 
-import Data.ByteString
-import Aoc.Day12.Part1
+import qualified Data.ByteString as BS
+import Data.List
 
-solve :: [ByteString] -> String
-solve = undefined
+import qualified Aoc.Day12.Part1 as P1
+
+solve :: [BS.ByteString] -> String
+solve = show . length . allPaths "start" "end" . map P1.readRow
+
+allPaths :: String -> String -> [(String, String)] -> [[String]]
+allPaths start end edges = allPaths' start end edges []
+
+allPaths' :: String -> String -> [P1.Edge] -> P1.Path -> [P1.Path]
+allPaths' start end edges visited
+  | start == end = [end:visited]
+  | start == "start" && not (null visited) = []
+  | P1.isSmallNode start &&
+    (start `elem` visited) &&
+    (length (nub $ filter P1.isSmallNode visited) /= length (filter P1.isSmallNode visited)) = []
+  | otherwise = concatMap (\s -> allPaths' s end edges (start:visited)) $ P1.adjNodes start edges
