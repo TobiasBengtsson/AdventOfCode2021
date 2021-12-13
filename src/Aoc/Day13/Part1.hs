2e49415d655461d9ea3fb36c3fb2b92f377d1506
Tobias Bengtsson <bo.tobias.bengtsson@gmail.com>
2021-12-13 07:27:02 +0100

    Day 13

--- src/Aoc/Day13/Part1.hs
+++ src/Aoc/Day13/Part1.hs
@@ -1,6 +1,40 @@
 module Aoc.Day13.Part1 where
 
-import Data.ByteString
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.Set as Set
+import Data.List (break)
+import Data.Maybe (fromJust)
 
-solve :: [ByteString] -> String
-solve = undefined
+solve :: [BS.ByteString] -> String
+solve s = show $ Set.size $ fold (head folds) (Set.fromList dots) 
+  where
+    parts = break (== "") s
+    dots  = readDots $ fst parts
+    folds = readFolds $ drop 1 $ snd parts
+
+fold :: Fold -> Set.Set (Int, Int) -> Set.Set (Int, Int)
+fold fld = Set.map fold'
+  where
+    fold' (x,y) = case fld of
+      X x' -> if x > x' then (x' - (x - x'), y) else (x, y)
+      Y y' -> if y > y' then (x, y' - (y - y')) else (x, y)
+
+readDot :: BS.ByteString -> (Int, Int)
+readDot s = (head is, is !! 1)
+  where
+    is = map (fst . fromJust . BC.readInt) $ BC.split ',' s
+
+readDots = map readDot
+
+data Fold = X Int | Y Int
+
+readFold :: BS.ByteString -> Fold
+readFold s = cons val
+  where
+    spl  = BC.split '=' s
+    spl' = BC.split ' ' (head spl)
+    cons = if last spl' == "x" then X else Y
+    val  = (fst . fromJust . BC.readInt) (last spl)
+
+readFolds = map readFold
--- src/Aoc/Day13/Part2.hs
+++ src/Aoc/Day13/Part2.hs
@@ -1,7 +1,22 @@
 module Aoc.Day13.Part2 where
 
-import Data.ByteString
+import qualified Data.ByteString as BS
+import qualified Data.Set as Set
+import Data.List (break)
+
 import Aoc.Day13.Part1
 
-solve :: [ByteString] -> String
-solve = undefined
+solve :: [BS.ByteString] -> String
+solve s = show $ render sln
+  where
+    parts = break (== "") s
+    dots  = readDots $ fst parts
+    folds = readFolds $ drop 1 $ snd parts
+    sln   = foldl (flip fold) (Set.fromList dots) folds
+
+render sln = snd $ foldl (\(y,s) r -> (y+1, s ++ "\n" ++ map (\x -> toChar (x,y)) r)) (0,"") grid
+  where
+    maxX  = foldr (\(x,y) m -> max x m) 0 sln
+    maxY  = foldr (\(x,y) m -> max y m) 0 sln
+    grid  = replicate (maxY + 1) [0..maxX]
+    toChar (x,y) = if (x,y) `elem` sln then '#' else '.'
