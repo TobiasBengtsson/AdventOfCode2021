9e55ad355d29f447db20343db0ffdd886ad58c96
Tobias Bengtsson <bo.tobias.bengtsson@gmail.com>
2021-12-17 07:14:14 +0100

    Day 17

--- src/Aoc/Day17/Part1.hs
+++ src/Aoc/Day17/Part1.hs
@@ -1,6 +1,18 @@
 module Aoc.Day17.Part1 where
+import Aoc.Day17.Part2 (minY)
 
-import Data.ByteString
+solve :: a -> String
+solve _ = show $ vy0 * (vy0 + 1) `div` 2
+  where
+    minY = -68
+    vy0  = -minY - 1
 
-solve :: [ByteString] -> String
-solve = undefined
+-- First observation: it is possible to reach vx=0 over the target area (solve n(n+1)/2 witin
+-- [minX, maxX] for integer solutions) which means the only limiting factor is in the y dimension.
+-- Because of symmetry, if the parable starts with a positive vy0 it will always pass x=0 on
+-- its way back.
+-- We want the downward velocity to be as large as possible just before hitting the target area,
+-- since this means we have maximized the height of the parable.
+-- Hence, the final step should be from x=0 to x=minY giving the final vy=0-minY.
+-- Again because of symmetry this gives a starting velocity of vy0 = -minY - 1.
+-- The maximum height is the sum vy0 + (vy0 - 1) .. 0 which is vy0(vy0 + 1)/2.
--- src/Aoc/Day17/Part2.hs
+++ src/Aoc/Day17/Part2.hs
@@ -1,7 +1,36 @@
 module Aoc.Day17.Part2 where
 
-import Data.ByteString
-import Aoc.Day17.Part1
+import Control.Monad
 
-solve :: [ByteString] -> String
-solve = undefined
+solve :: a -> String
+solve _ = show $ length $ filter (possibleVXY (0,0)) $ possibleVXs × possibleVYs
+
+a × b = liftM2 (,) a b
+
+possibleVXs = filter (possibleVX 0) [0..maxX]
+
+possibleVX x vx
+ | x >= minX && x <= maxX = True
+ | x > maxX               = False
+ | vx == 0                = False
+ | otherwise              = possibleVX (x + vx) (vx - 1)
+
+possibleVYs = filter (possibleVY 0) [minY, (minY + 1)..(-minY)]
+
+possibleVY y vy
+ | y >= minY && y <= maxY = True
+ | y < minY               = False
+ | otherwise              = possibleVY (y + vy) (vy - 1)
+
+possibleVXY (x,y) (vx, vy)
+ | x >= minX && x <= maxX
+   && y >= minY && y <= maxY = True
+ | x > maxX                  = False
+ | y < minY                  = False
+ | otherwise                 = possibleVXY (x + vx, y + vy) (max (vx - 1) 0, vy - 1)
+
+minX = 269
+maxX = 292
+
+maxY = -44
+minY = -68
