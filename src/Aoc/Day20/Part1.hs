9a8e2e2607b8a02ba0651ad6fa3f21069832b01c
Tobias Bengtsson <bo.tobias.bengtsson@gmail.com>
2021-12-20 08:24:14 +0100

    Day 20: refactor and more general solution

--- src/Aoc/Day20/Part1.hs
+++ src/Aoc/Day20/Part1.hs
@@ -6,41 +6,67 @@ import Data.List (sortOn)
 import qualified Data.Map as Map
 import Data.Maybe
 import qualified Data.Set as Set
-import Debug.Trace
 
 type Coord = (Int, Int)
-data PixelState = Dark | Lit deriving (Eq, Show)
-data Image = Image
-  { coords      :: Set.Set Coord
-  , boundaryMin :: Coord
-  , boundaryMax :: Coord
-  , defState    :: PixelState }
-  deriving Show
 
-type Algo = Map.Map Int PixelState
+adjCoords :: Coord -> [Coord]
+adjCoords (x, y) = sortOn snd $ sortOn fst [(x+x',y+y') | x' <- [-1,0,1], y' <- [-1,0,1]]
 
-cToPixelState '#' = Lit
-cToPixelState '.' = Dark
-cToPixelState _   = undefined
+
+data PixelState = Dark | Lit deriving Eq
+
+readPs '#' = Lit
+readPs '.' = Dark
+readPs _   = undefined
+
+showPs Lit  = '#'
+showPs Dark = '.'
 
 toBin :: PixelState -> Int
 toBin Lit  = 1
 toBin Dark = 0
 
+
+type Algo = Map.Map Int PixelState
+
+readAlgo :: BS.ByteString -> Map.Map Int PixelState
+readAlgo = Map.fromList . zip [0..] . map readPs .  BC.unpack
+
+
+data Image = Image
+  { litCoords :: Set.Set Coord
+  , minX      :: Int
+  , minY      :: Int
+  , maxX      :: Int
+  , maxY      :: Int
+  , defState  :: PixelState }
+
+readImg :: [BS.ByteString] -> Image
+readImg bs = Image
+  { litCoords = foldl (\set (y, l) -> Set.union (readImgRow y l) set) Set.empty $ zip [0..] bs
+  , minX      = 0
+  , maxX      = maximum (map BS.length bs) - 1
+  , minY      = 0
+  , maxY      = length bs - 1
+  , defState  = Dark }
+
+readImgRow :: Int -> BS.ByteString -> Set.Set Coord
+readImgRow y = foldl (\set (x, c) -> insertIfLit (x,y) (readPs c) set) Set.empty . zip [0..] . BC.unpack
+
+showImg :: Image -> String
+showImg img = unlines $ map (\y -> map (\x -> showPs (psLookup (x,y) img)) [minX img..maxX img]) [minY img..maxY img]
+
 inBoundary :: Coord -> Image -> Bool
-inBoundary (x,y) img = x >= (fst $ boundaryMin img)
-                    && x <= (fst $ boundaryMax img)
-                    && y >= (snd $ boundaryMin img)
-                    && y <= (snd $ boundaryMax img)
+inBoundary (x,y) img = x >= minX img && x <= maxX img && y >= minY img && y <= maxY img
 
 psLookup :: Coord -> Image -> PixelState
 psLookup coord img
-  | not $ inBoundary coord img    = defState img
-  | Set.member coord (coords img) = Lit
-  | otherwise                     = Dark
+  | not $ inBoundary coord img       = defState img
+  | Set.member coord (litCoords img) = Lit
+  | otherwise                        = Dark
 
-insertPs :: Coord -> PixelState -> Set.Set Coord -> Set.Set Coord
-insertPs c ps = if ps == Lit then Set.insert c else id
+insertIfLit :: Coord -> PixelState -> Set.Set Coord -> Set.Set Coord
+insertIfLit c ps = if ps == Lit then Set.insert c else id
 
 enhancePixel :: Coord -> Algo -> Image -> PixelState
 enhancePixel c algo img = res
@@ -51,36 +77,27 @@ enhancePixel c algo img = res
 
 enhanceImage :: Algo -> Image -> Image
 enhanceImage algo img = Image
-  { coords = foldl (\newCoords c -> insertPs c (enhancePixel c algo img) newCoords) Set.empty allCoords
-  , boundaryMin = (minX, minY)
-  , boundaryMax = (maxX, maxY)
-  , defState    = if defState img == Lit then Dark else Lit }
+  { litCoords = foldl (\newCoords c -> insertIfLit c (enhancePixel c algo img) newCoords) Set.empty allCoords
+  , minX = newMinX
+  , maxX = newMaxX
+  , minY = newMinY
+  , maxY = newMaxY
+  , defState    = if defState img == Lit && snd (fromJust (Map.lookupMax algo)) == Dark then Dark else
+                  if defState img == Dark && snd (fromJust (Map.lookupMin algo)) == Lit then Lit else
+                     defState img }
   where
-    minX = (fst $ boundaryMin img) - 1
-    maxX = (fst $ boundaryMax img) + 1
-    minY = (snd $ boundaryMin img) - 1
-    maxY = (snd $ boundaryMax img) + 1
-    allCoords = [(x,y) | x <- [minX..maxX], y <- [minY..maxY]]
+    newMinX = minX img - 1
+    newMaxX = maxX img + 1
+    newMinY = minY img - 1
+    newMaxY = maxY img + 1
+    allCoords = [(x,y) | x <- [newMinX..newMaxX], y <- [newMinY..newMaxY]]
 
-adjCoords :: Coord -> [Coord]
-adjCoords (x, y) = sortOn snd $ sortOn fst [(x+x',y+y') | x' <- [-1,0,1], y' <- [-1,0,1]]
 
 solve :: [BS.ByteString] -> String
 solve = solve' 2
 
 solve' :: Int -> [BS.ByteString] -> String
-solve' n bs = show $ Set.size $ coords $ iterate (enhanceImage algo) image !! n
+solve' n bs = show $ Set.size $ litCoords $ iterate (enhanceImage algo) image !! n
   where
     algo = readAlgo $ head bs
-    image = readImage $ drop 2 bs
-
-readAlgo :: BS.ByteString -> Map.Map Int PixelState
-readAlgo = Map.fromList . zip [0..] . map cToPixelState .  BC.unpack
-
-readImage :: [BS.ByteString] -> Image
-readImage bs = Image { coords = cs, boundaryMin = (0,0), boundaryMax = (99,99), defState = Dark }
-  where
-    cs = foldl (\set (y, l) -> Set.union (readImageRow y l) set) Set.empty $ zip [0..] bs
-
-readImageRow :: Int -> BS.ByteString -> Set.Set Coord
-readImageRow y = foldl (\set (x, c) -> if cToPixelState c == Lit then Set.insert (x, y) set else set) Set.empty . zip [0..] . BC.unpack
+    image = readImg $ drop 2 bs
