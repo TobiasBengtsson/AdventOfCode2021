d93234a991c3384ea5e8e32d569e1c392bd53385
Tobias Bengtsson <bo.tobias.bengtsson@gmail.com>
2021-12-21 21:16:09 +0100

    Day 21

--- src/Aoc/Day21/Part1.hs
+++ src/Aoc/Day21/Part1.hs
@@ -1,6 +1,39 @@
 module Aoc.Day21.Part1 where
 
-import Data.ByteString
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.Char as Char
+import Control.Monad.State
 
-solve :: [ByteString] -> String
-solve = undefined
+data GameState = GameState
+  { p1Score  :: Int
+  , p2Score  :: Int
+  , p1Pos    :: Int
+  , p2Pos    :: Int
+  , nextDies :: [Int]
+  , throwCount :: Int }
+
+solve :: [BC.ByteString] -> String
+solve bs = show $ evalState runGame $ GameState { p1Score = 0, p2Score = 0, p1Pos = p1Start, p2Pos = p2Start, nextDies = deterministicDie, throwCount = 0 }
+  where
+    p1Start = readStart $ head bs
+    p2Start = readStart $ bs !! 1
+
+runGame :: State GameState Int
+runGame = do
+  s <- get
+  let p1Step = sum $ take 3 (nextDies s)
+  let p1NewPos = ((p1Pos s + p1Step - 1) `mod` 10) + 1
+  let p1NewScore = p1Score s + p1NewPos
+  if p1NewScore >= 1000 then return (p2Score s * (throwCount s + 3)) else do
+    let p2Step = sum $ take 3 $ drop 3 $ nextDies s
+    let p2NewPos = ((p2Pos s + p2Step - 1) `mod` 10) + 1
+    let p2NewScore = p2Score s + p2NewPos
+    if p2NewScore >= 1000 then return (p1NewScore * (throwCount s + 6)) else do
+      put s { p1Score = p1NewScore, p2Score = p2NewScore, p1Pos = p1NewPos, p2Pos = p2NewPos, nextDies = drop 6 $ nextDies s, throwCount = throwCount s + 6 }
+      runGame
+
+deterministicDie :: [Int]
+deterministicDie = cycle [1..100]
+
+readStart :: BC.ByteString -> Int
+readStart = Char.digitToInt . BC.last
--- src/Aoc/Day21/Part2.hs
+++ src/Aoc/Day21/Part2.hs
@@ -1,7 +1,62 @@
 module Aoc.Day21.Part2 where
 
-import Data.ByteString
-import Aoc.Day21.Part1
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.Char as Char
+import qualified Data.Map as Map
+import Control.Monad.State
+import Debug.Trace
 
-solve :: [ByteString] -> String
-solve = undefined
+data GameState = GameState
+  { p1Score  :: Int
+  , p2Score  :: Int
+  , p1Pos    :: Int
+  , p2Pos    :: Int
+  , turn     :: Int }
+  deriving (Eq, Ord)
+
+solve :: [BC.ByteString] -> String
+solve bs = show $ tplMax $ evalState (runGame initState) Map.empty
+  where
+    p1Start = readStart $ head bs
+    p2Start = readStart $ bs !! 1
+    initState = GameState
+      { p1Score = 0
+      , p2Score = 0
+      , p1Pos = p1Start
+      , p2Pos = p2Start
+      , turn = 1 }
+
+runGame :: GameState -> State (Map.Map GameState (Int, Int)) (Int, Int)
+runGame s
+  | p1Score s >= 21 = return (1,0)
+  | p2Score s >= 21 = return (0,1)
+  | otherwise = do
+      memo <- get
+      case Map.lookup s memo of
+        Just x  -> return x
+        Nothing -> do
+          let quantumStates = mapM (runGame . (`move` s)) combinations
+          thisStateScore <- foldl1 tplAdd <$> quantumStates
+          modify $ Map.insert s thisStateScore
+          return thisStateScore
+
+move :: Int -> GameState -> GameState
+move steps s
+  | turn s == 1 = let p = newPos (p1Pos s) in
+                    s { p1Score = p1Score s + p, p1Pos = p, turn = 2 }
+  | otherwise   = let p = newPos (p2Pos s) in
+                    s { p2Score = p2Score s + p, p2Pos = p, turn = 1 }
+  where
+    newPos pos = (pos + steps - 1) `mod` 10 + 1
+
+combinations :: [Int]
+combinations = [x + y + z | x <- [1..3], y <- [1..3], z <- [1..3]]
+
+readStart :: BC.ByteString -> Int
+readStart = Char.digitToInt . BC.last
+
+tplMax :: (Int, Int) -> Int
+tplMax (a,b) = max a b
+
+tplAdd :: (Int, Int) -> (Int, Int) -> (Int, Int)
+tplAdd (a,b) (a',b') = (a+a',b+b')
